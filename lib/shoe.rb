bdf1ca37c31104efbf5c135b16b24c656c383a57
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-07-10 20:31:03 -0400

    Completed - add official soln. as an alternative to the original working soln. already committed.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,22 @@
+# Added the official solution as an alternative to the 
+# originally written working solution shown below:
+#
+# More tests have to be done to find out which solution 
+# is faster for large and continuously updated libraries
+# of brands.
+#
+# For this case, it could be that the official solution
+# is faster.
+#
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
   BRANDS = []
 
-  def initialize(brand)
+  # original
+  def initialize_0(brand)
     @brand = brand
     BRANDS << brand
     BRANDS.uniq!
@@ -15,4 +27,9 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+  # official solution
+  def initialize(brand)
+    @brand = brand
+    BRANDS << brand unless BRANDS.include?(brand)
+  end
 end
